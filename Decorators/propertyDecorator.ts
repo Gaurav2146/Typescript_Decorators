830dbbba01c7474cde8fc02f7eed0d6201844a37
Gaurav Kumar <GauravKumar@WinjitTechnologiesPvtLtd.local>
2023-07-31 11:06:00 +0530

    Property decorators code modified

--- Decorators/propertyDecorator.ts
+++ Decorators/propertyDecorator.ts
@@ -1,12 +1,12 @@
 
 class Greeter {
 
-	@ModifyMessage("GFG")
-	first_message: string;
-	second_message: string;
+    @ModifyMessage("GFG")
+    first_message: string;
+    second_message: string;
 
-    constructor(first_message:string,second_message:string)
-    {   this.first_message = first_message;
+    constructor(first_message: string, second_message: string) {
+        this.first_message = first_message;
         this.second_message = second_message;
     }
 }
@@ -14,35 +14,40 @@ class Greeter {
 
 //Decorator factory
 function ModifyMessage(sender: string) {
-  
-    return function(target: any, propertyKey: string) {
-      
-        let modifiedMessage : string;
-          
+
+    return function (target: Greeter, propertyKey: string) {
+
+        let modifiedMessage: string;
+
         // Return modifiedMessage whenever the message is asked
-        const getter = function() {
-            return modifiedMessage + " this message in fethed from getter";
+        const getter = function () {
+            return modifiedMessage + " this message is fethed from getter";
         };
-          
+
         // Set the modifiedMessage value
-        const setter = function(message:string) {
-            if(message)
-            {
-                modifiedMessage = message + " This message id set inside setter"; 
+        const setter = function (message: string) {
+            if (message) {
+                modifiedMessage = message + " This message is set inside setter";
             }
-        }; 
-  
-        // Overwrite the original message with
-        // modifiedMessage we just created
+        };
+
+        // adding getter and setter for the property
         Object.defineProperty(target, propertyKey, {
             get: getter,
             set: setter
         });
+
+        //Adding value to the property and making value redable.
+        //So, If you try to set the value then it will throw an error even in constructor.
+        // Object.defineProperty(target, propertyKey, {
+        //     value: "This is custom value",
+        //     writable: false
+        // });
     }
 }
 
-let greeter:Greeter = new Greeter("Hello","How are you");
+let greeter: Greeter = new Greeter("Hello", "How are you");
 
 console.log(greeter.first_message);
-greeter.first_message="Hello from Gaurav";
+greeter.first_message = "Hello from Gaurav";
 console.log(greeter.first_message);
